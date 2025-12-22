f13867e78b25ead48fe5a30442d519ab50dcf6e4
dburq <dburquezeducation@gmail.com>
2025-12-22 02:47:02 -0600

    Create Player Instance and cleaning up main

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import pygame
 from constants import *
+from player import *
 from logger import log_state
 
+FPS = 60
+
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    infloop = 0
-    while infloop < 1:
+    clock = pygame.time.Clock()
+      
+    running = True 
+    while running:
         log_state()
+        dt = clock.tick(FPS) / 1000
+        player_sprite = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+        
         screen.fill("black")
+        player_sprite.draw(screen)
         pygame.display.flip()
-        infloop -= 1
+        
+
+        
+    pygame.quit()       
 
 if __name__ == "__main__":
     main()
