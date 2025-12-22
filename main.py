fbfeecc0d8af3df66c51d304dd300d1d5537574c
dburq <dburquezeducation@gmail.com>
2025-12-22 01:59:59 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    infloop = 0
+    while infloop < 1:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        infloop -= 1
+
+if __name__ == "__main__":
+    main()
